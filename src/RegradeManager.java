0f49a884f0ab5529926f1e52a23332e820c2b7f9
Eric Asare <70132474+eric-asare@users.noreply.github.com>
2023-12-05 16:17:55 -0500

    regradeManager implementation

--- src/RegradeManager.java
+++ src/RegradeManager.java
@@ -1,35 +1,88 @@
-import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class RegradeManager {
-  private static int requestcount;
+    private static int requestcount;
+    private static ArrayList<String> filepaths;
 
-  //String[] questions
-  //String[] answers
-  //String[] grades
+    public RegradeManager() {
+        filepaths = new ArrayList<>();
+        requestcount = 0;
+    }
 
-  private static ArrayList<String> filepaths;
+    public static void save(String requestText, int examID, int studentID, int question_no) {
+        if (filepaths == null) {
+            filepaths = new ArrayList<>();
+        }
+        
+        int requestID = requestcount;
+        requestcount++;
+        String path = "requests/" + Integer.toString(studentID) +  "," + Integer.toString(examID) +  ","+ Integer.toString(studentID) +  "," + Integer.toString(requestID);
+        filepaths.add(path);
+        String status = "open";
+        writeToFile(path, Integer.toString(question_no), requestText, status);
+    }
 
-  public RegradeManager(){
+    private static void writeToFile(String filePath, String... content) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
+
+            // Writing content to the file
+            for (String str : content) {
+                writer.write(str);
+            }
+
+            // Closing the writer
+            writer.close();
+
+            System.out.println("Content has been written to the file successfully.");
+        } catch (IOException e) {
+            System.err.println("Error writing to the file: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public static void get_requests_display_info() {
+      if (filepaths == null || filepaths.isEmpty()) {
+          System.out.println("No requests available.");
+          return;
+      }
+
+      List<String> formattedInfoList = new ArrayList<>();
+      for (String path : filepaths) {
+          String[] parts = path.split(",");
+          if (parts.length == 4) {
+              int studentID = Integer.parseInt(parts[0].substring(parts[0].indexOf("/") + 1)); // Extracting student ID
+              int examID = Integer.parseInt(parts[1]);
+              int requestStudentID = Integer.parseInt(parts[2]);
+              int requestID = Integer.parseInt(parts[3]);
+
+              String formattedInfo = String.format("Exam ID: %d, Student ID: %d, Request Student ID: %d, Request ID: %d",
+                      examID, studentID, requestStudentID, requestID);
+              formattedInfoList.add(formattedInfo);
+          }
+      }
+
+      if (!formattedInfoList.isEmpty()) {
+          System.out.println("Requests Information:");
+          for (String info : formattedInfoList) {
+              System.out.println(info);
+          }
+      } else {
+          System.out.println("No requests information available.");
+      }
   }
 
-  public static void save(String requestText, int examID, int studentID, int question_no) {
-    int requestid = request_count;
-    request_count++;
-    // String path =  source +studentId_examID_requestID
-    // store question number + requestInfo in 
-    //file write
-  }
 
-  public static void get_requests_display_info() {
-    // parse the filepaths, return formated string array examid, studentid, question_no, status
-    
-  }
-  // source+studentId_examID_question_no
-  public static void open_request_file(int examID, int studentID, int request_id) {
-    //return content of file - requestInfo
-    // filepath = filepaths.get(examID, studentID, requestID);
-    requestText = "info from file";
-  }
 
+    public static void main(String[] args) {
+        RegradeManager.save("Request text", 123, 456, 789);
+        RegradeManager.save("Request text", 123, 456, 89);
+        RegradeManager.get_requests_display_info();
+    }
 }
+
+
